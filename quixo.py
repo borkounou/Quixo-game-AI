f838be829d8914f7f0846b0c1076e966bb82c932
Cherif hassan <nousradinecherifhassane@gmail.com>
2022-03-18 01:26:18 +0100

    working final

--- quixo.py
+++ quixo.py
@@ -17,8 +17,7 @@ class Board:
         self.empty_sqrs = self.squares # [squares]
         self.mark_sqrs = 0
 
-    
-    def final_state(self):
+    def final_state(self, show=False):
         '''
         @return 0 if there is  no win yet
         @return 1 if player 1 wins
@@ -27,23 +26,38 @@ class Board:
         #Vertical wins
         for col in range(COLS):
             if self.squares[0][col] == self.squares[1][col] == self.squares[2][col]== self.squares[3][col]==self.squares[4][col]!=0:
+                if show:
+                    color = CIRC_COLOR if self.squares[0][col] ==2 else CROSS_COLOR
+                    iPos = (col * SQSIZE + SQSIZE//2, 20)
+                    fPos = (col * SQSIZE + SQSIZE//2, HEIGHT-20)
+                    pygame.draw.line(screen, color, iPos, fPos, LINE_WIDTH)
                 return self.squares[0][col]
 
         #Horizontal wins
         for row in range(ROWS):
             if self.squares[row][0] == self.squares[row][1] == self.squares[row][2]== self.squares[row][3]==self.squares[row][4]!=0:
+                if show:
+                    color = CIRC_COLOR if self.squares[row][0] ==2 else CROSS_COLOR
+                    iPos = ( 20, row * SQSIZE + SQSIZE//2)
+                    fPos = (WIDTH-20, row * SQSIZE + SQSIZE//2)
+                    pygame.draw.line(screen, color, iPos, fPos, LINE_WIDTH)
                 return self.squares[row][0]
-
         
         # Desc diagonal 
         if self.squares[0][0] == self.squares[1][1] == self.squares[2][2]==self.squares[3][3] ==self.squares[4][4] !=0:
+            if show:
+                    color = CIRC_COLOR if self.squares[2][2] ==2 else CROSS_COLOR
+                    iPos = ( 20,20)
+                    fPos = (WIDTH-20, HEIGHT-20)
             return  self.squares[2][2]
 
-        
         # Asc diagonal 
 
         if self.squares[4][0] == self.squares[3][1] == self.squares[2][2]==self.squares[1][3] ==self.squares[0][4] !=0:
-
+            if show:
+                    color = CIRC_COLOR if self.squares[2][2] ==2 else CROSS_COLOR
+                    iPos = (20,HEIGHT-20)
+                    fPos = (WIDTH-20, 20)
             return  self.squares[2][2]
         
         # no win yet
@@ -80,7 +94,7 @@ class Board:
 
 class AI:
 
-    def __init__(self, level=1, player=2):
+    def __init__(self, level=0, player=2):
         self.level = level
         self.player = player
 
@@ -120,10 +134,9 @@ class AI:
                 temp_board = copy.deepcopy(board)
                 temp_board.mark_sqr(row, col, 1)
                 eval = self.minimax(temp_board, False)[0]
-                if eval > max_eval:
+                if eval >= max_eval:
                     max_eval = eval
                     best_move = (row, col)
-
             return max_eval, best_move
 
         elif not maximizing:
@@ -135,10 +148,9 @@ class AI:
                 temp_board = copy.deepcopy(board)
                 temp_board.mark_sqr(row, col, self.player)
                 eval = self.minimax(temp_board, True)[0]
-                if eval < min_eval:
+                if eval <= min_eval:
                     min_eval = eval
                     best_move = (row, col)
-
             return min_eval, best_move
 
     # --- MAIN EVAL ---
@@ -160,12 +172,18 @@ class Game:
     def __init__(self):
         self.board = Board()
         self.ai = AI()
-        self.player = 1 # 1-cross # 2-circle
+        self.player = 2# 1-cross # 2-circle
         self.gamemode = 'ai' # pvp or ai
         self.running  = True
         self.show_lines()
+    
+    def make_move(self, row, col):
+        self.board.mark_sqr(row, col, self.player)
+        self.draw_fig(row, col)
+        self.next_turn()
 
     def show_lines(self):
+        screen.fill(BG_COLOR)
         # Vertical lines
         pygame.draw.line(screen, LINE_COLOR,(SQSIZE,0),(SQSIZE, HEIGHT),LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR,(WIDTH-SQSIZE,0),(WIDTH-SQSIZE,HEIGHT),LINE_WIDTH)
@@ -203,9 +221,16 @@ class Game:
     def next_turn(self):
         self.player = self.player % 2 + 1
 
+    def change_gamemode(self):
+        self.gamemode='ai' if self.gamemode=='pvp' else 'pvp'
 
+    
+    def reset(self):
+        self.__init__()
 
-        
+    def isover(self):
+        return self.board.final_state(show=True)!=0 or self.board.isfull()
+
 
 
 def main():
@@ -220,25 +245,43 @@ def main():
             if event.type ==pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                #g-gamemode
+                if event.key == pygame.K_g:
+                    game.change_gamemode()
+
+                if event.key == pygame.K_r:
+                    game.reset()
+                    board = game.board
+                    ai = game.ai
+
+                
+                if event.key ==pygame.K_0:
+                    ai.level = 0
+                if event.key ==pygame.K_1:
+                    ai.level = 1
             
             if event.type ==pygame.MOUSEBUTTONDOWN:
                 pos = event.pos
                 row = pos[1]//SQSIZE
                 col = pos[0]//SQSIZE
                 
-                if board.empty_sqr(row, col):
-                    board.mark_sqr(row,col,game.player)
-                    game.draw_fig(row,col)
-                    game.next_turn()
+                if board.empty_sqr(row, col) and game.running:
+                    game.make_move(row, col)
                     # print(board.squares)
-        if game.gamemode =='ai' and game.player == ai.player:
+                    if game.isover():
+                        game.running = False
+
+
+        if game.gamemode =='ai' and game.player == ai.player and game.running:
             pygame.display.update()
 
             # ai methods
             row, col = ai.eval(board)
-            board.mark_sqr(row,col,game.player)
-            game.draw_fig(row,col)
-            game.next_turn()
+            game.make_move(row, col)
+            if game.isover():
+                game.running = False
 
 
     
